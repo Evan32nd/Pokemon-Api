0920d766c4bfd4d79bc5bc9652533fc4078deb57
Evan32nd <eqlh1998@gmail.com>
2022-12-19 12:18:20 -0700

    website is working as intended

--- pokemon.js
+++ pokemon.js
@@ -2,6 +2,29 @@
 
 const poke_container = document.getElementById("poke_container");
 const pokemon_number = 150;
+const colors = {
+  bug: "#f8d5a3",
+  dark: "#",
+  dragon: "#97b3e6",
+  electric: "#FCF7DE",
+  fairy: "#fceaff",
+  fighting: "#E6E0D4",
+  fire: "#FDDFDF",
+  flying: "#c1e0e0",
+  ghost: "#",
+  grass: "#DEFDE0",
+  ground: "#f4e7da",
+  ice: "#",
+  normal: "#F5F5F5",
+  poison: "#89d7a5",
+  psychic: "#eaeda1",
+  rock: "#d5d5d4",
+  steel: "#",
+  water: "#DEF3DF",
+
+};
+
+const main_types = Object.keys(colors);
 
 const fetchPokemon = async () => {
   for (let i = 1; i < pokemon_number; i++) {
@@ -22,19 +45,26 @@ function createPokemonCard(pokemon) {
   const pokemonEl = document.createElement("div");
   pokemonEl.classList.add("pokemon");
 
+  const poke_types = pokemon.types.map((el) => el.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+
   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
   const id = pokemon.id;
-  const image = pokemon.sprite;
-  const type = pokemon.type;
+  const color = colors[type];
+  console.log(color);
+
+  pokemonEl.style.backgroundColor = color;
 
   const pokeInnerHTML =
     '<div class="img-container"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' +
     id +
-    '.png"></div><div class="info"> <span class="number">' +
-    id +
+    '.png"></div><div class="info"> <span class="number">#' +
+    id.toString().padStart(3, '0') +
     '</span><h3 class="name">' +
     name +
-    '</h3><small class="type">Type: <span>' + type + '</span></small></div>';
+    '</h3><small class="type">Type: <span>' +
+    type +
+    "</span></small></div>";
 
   //pokemondb.net/pokedex/bulbasaur
 
